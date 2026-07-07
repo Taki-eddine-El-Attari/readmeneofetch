62f1f0126a5dac310100ada99a97074d3f46a7ab
Taki Eddine <takieddineelattari@gmail.com>
2026-07-07 16:51:09 +0100

    Refactor ASCII art generation functions
    Updated image_to_ascii function to improve aspect ratio handling and contrast. Enhanced get_ascii_char function and adjusted ASCII character set.

--- src/draw_ascii.py
+++ src/draw_ascii.py
@@ -1,34 +1,33 @@
 import requests
 from github import Github
-from PIL import Image
+from PIL import Image, ImageOps
 from io import BytesIO
-from src.fetch_info import fetch_stats
 
-def get_ascii_char(pixel):
-    ascii_chars = '.:-=+*#%@'
-    brightness = sum(pixel)/3
-    char_index = min(int(brightness/255 * (len(ascii_chars)-1)), len(ascii_chars)-1)
-    return ascii_chars[char_index]
+ASCII_CHARS = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
-def image_to_ascii(image, width=50) -> str:
-    aspect_ratio = image.width / image.height
-    height = int((width*aspect_ratio)*0.5)
+def get_ascii_char(brightness):
+    char_index = min(int(brightness / 255 * (len(ASCII_CHARS) - 1)), len(ASCII_CHARS) - 1)
+    return ASCII_CHARS[char_index]
+
+def image_to_ascii(image, width=100) -> str:
+    aspect_ratio = image.height / image.width
+    height = int(width * aspect_ratio * 0.5)
 
     image = image.resize((width, height))
-    image = image.convert('RGB')
-    ascii_str = ""
+    image = image.convert('L')          # vraie luminosité, pas juste moyenne RGB
+    image = ImageOps.autocontrast(image)  # accentue le contraste
 
-    for y in range(height): 
+    ascii_str = ""
+    for y in range(height):
         for x in range(width):
-            pixel = image.getpixel((x,y))
-            ascii_str += get_ascii_char(pixel)
+            brightness = image.getpixel((x, y))
+            ascii_str += get_ascii_char(brightness)
         ascii_str += "\n"
 
     return ascii_str
 
-def generate_logo(g:Github) -> str:
+def generate_logo(g: Github, width=100) -> str:
     user_pfp = g.get_user().avatar_url
     response = requests.get(user_pfp)
     img = Image.open(BytesIO(response.content))
-
-    return image_to_ascii(img)
+    return image_to_ascii(img, width)
